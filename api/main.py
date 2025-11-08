454e6b5c8f90811cdbca2a3424407fada2bfd0b3
LittleBoyzzz <veerapat.sakphet@gmail.com>
2025-11-08 14:38:03 +0700

    tar

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.schemas import WordResponse
 from fastapi import HTTPException
-from app.routers import words
+from app.routers import words,practice
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -10,12 +11,22 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(
     words.router,
     prefix="/api",
     tags=["words"]
 
 )
+
+app.include_router(practice.router,prefix="/api",tags=["practice"])
 
 #@app.get("/api/word", response_model=WordResponse)
 #def get_random_word():
